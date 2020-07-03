d6fea62099817b09a1f70930fc041b0a439b3933
Prescott Rowe <prescottrowe@gmail.com>
2020-07-02 22:39:01 -0400

    Update gocuty.sh

--- gocuty.sh
+++ gocuty.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "-------Gobuster meets cutycapt-----"
-echo "File will be saved into a gocuty folder"
+echo "Your files will be in the gocuty folder. Also make sure to edit the gobuster command on line 11 to your preference."
 echo "Enter your target URL/IP:"
+
 read scan
 echo "Scanning $scan:"
 rm -r gocuty/ 2>/dev/null
@@ -10,9 +11,9 @@ mkdir gocuty
 gobuster dir -t 50 -w /usr/share/seclists/Discovery/Web-Content/common.txt -x .txt,.php,.html,.yml -u $scan -e | tee ./gocuty/found.txt;
 i=0
 echo "<HTML><BODY><BR>" > ./gocuty/gocuty.html
-for url in $(cat found.txt |grep "Status: 200" |cut -d" " -f1);do
+for url in $(cat ./gocuty/found.txt |grep "Status: 200\|Status: 204\|Status: 301\|Status: 302\|Status: 307\|Status: 403" |cut -d" " -f1);do
 	((i++))
-	cutycapt --url=$url --out=$i.png
-	echo "<b>"$url":</b> <BR><IMG SRC=\""./gocuty/""$i"".png"\" width=600 border="6"><BR>" >> ./gocuty/gocuty.html
-echo "</BODY></HTML>" >> ./gocuty/gocuty.html
+	cutycapt --url=$url --out=./gocuty/$i.png
+	echo "<b>"$url":</b> <BR><IMG SRC=\""$i"".png"\" width=600 border="6"><BR><BR><BR>" >> ./gocuty/gocuty.html
 done
+echo "</BODY></HTML>" >> ./gocuty/gocuty.html
